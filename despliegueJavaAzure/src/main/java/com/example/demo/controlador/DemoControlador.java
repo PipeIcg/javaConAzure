913020be507e3bd2a2f9649b708a4486d86801ad
fcastro <fcastro@NT-Fcastro.scd.cl>
2023-06-27 23:10:55 -0400

    commit para prueba de despliegue

---
+++ despliegueJavaAzure/src/main/java/com/example/demo/controlador/DemoControlador.java
@@ -0,0 +1,13 @@
+package com.example.demo.controlador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoControlador {
+
+	@GetMapping("/mensaje")
+	public String mensaje() {
+		return "Hola mundo con azure :D";
+	}
+}
